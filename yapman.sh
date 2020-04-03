16d53e7e59d47446643839490158e16d24dae375
A. Taha Baki <a.taha.baki@outlook.com>
2020-04-03 21:10:18 +0300

    security + pckg removing...

--- yapman.sh
+++ yapman.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 BOLD="\033[1m"
 BOLDB="\033[1;34m"
 BOLDR="\033[1;31m"
@@ -9,11 +9,13 @@ OK="${BOLDG}[OK]${NORMAL}"
 ERR="${BOLDR}[ERR]${NORMAL}"
 WRN="${BOLDY}[WRN]${NORMAL}"
 
-YapmanPath="${HOME}/.yapman"
-YapmanConfigPath="${YapmanPath}/yapman.conf"
-YapmanCachePath="${YapmanPath}/cache"
-YapmanPackagePath="${YapmanPath}/packages"
-YapmanLogsPath="${YapmanPath}/logs"
+XDG_CONFIG_HOME="${HOME}/.config/yapman"
+XDG_CACHE_HOME="${HOME}/.cache/yapman"
+XDG_DATA_HOME="${HOME}/.local/share/yapman"
+YapmanConfigPath="${XDG_CONFIG_HOME}/yapman.conf"
+YapmanCachePath="${XDG_CACHE_HOME}/"
+YapmanPackagePath="${XDG_DATA_HOME}/packages"
+YapmanLogsPath="${XDG_CACHE_HOME}/logs"
 
 AUR_DOMAIN="https://aur.archlinux.org"
 AUR_BASE_URL="https://aur.archlinux.org/rpc/?v=5&type="
@@ -26,59 +28,103 @@ VERSION="2.0.0"
 ABBR="Yet Another Package Manager for AUR"
 
 intro() {
-	echo -e "${BOLDB}${PROGRAM}${NORMAL} version ${BOLDG}${VERSION}${NORMAL} by ${BOLDR}${AUTHOR}${NORMAL}"
-	echo -e "${BOLD}${ABBR}${NORMAL}\n"
+	"$echo" -e "${BOLDB}${PROGRAM}${NORMAL} version ${BOLDG}${VERSION}${NORMAL} by ${BOLDR}${AUTHOR}${NORMAL}"
+	"$echo" -e "${BOLD}${ABBR}${NORMAL}\n"
 }
 
 can_run() {
-	echo -e "${BOLD}Checking dependencies...${NORMAL}"
+	if [ -e "$(which sudo)" ]
+	then
+		sudo="$(which sudo)"
+	else
+		print_err "${BOLD}sudo${NORMAL} is not installed. You need it to run this."
+		exit 127
+	fi
 	if [ -e "$(which pacman)" ]
 	then
-		print_ok "${BOLD}pacman${NORMAL} is installed."
+		pacman="$(which pacman)"
 	else
 		print_err "${BOLD}pacman${NORMAL} is not installed. You need it to run this."
 		exit 127
 	fi
+	if [ -e "$(which makepkg)" ]
+	then
+		makepkg="$(which makepkg)"
+	else
+		print_err "${BOLD}makepkg${NORMAL} is not installed. You need it to run this."
+		exit 127
+	fi
 	if [ -e "$(which jq)" ]
 	then
-		print_ok "${BOLD}jq${NORMAL} is installed."
+		jq="$(which jq)"
 	else
 		print_err "${BOLD}jq${NORMAL} is not installed. You need it to run this."
 		exit 127
 	fi
+	if [ -e "$(which git)" ]
+	then
+		git="$(which git)"
+	else
+		print_err "${BOLD}git${NORMAL} is not installed. You need it to run this."
+		exit 127
+	fi
 	if [ -e "$(which bash)" ]
 	then
-		print_ok "${BOLD}bash${NORMAL} is installed."
+		bash="$(which bash)"
 	else
 		print_err "${BOLD}bash${NORMAL} is not installed. You need it to run this."
 		exit 127
 	fi
+	if [ -e "$(which mkdir)" ]
+	then
+		mkdir="$(which mkdir)"
+	else
+		print_err "${BOLD}mkdir${NORMAL} is not installed. You need it to run this."
+	fi
+	if [ -e "$(which rm)" ]
+	then
+		rm="$(which rm)"
+	else
+		print_err "${BOLD}rm${NORMAL} is not installed. You need it to run this."
+	fi
+	if [ -e "$(which touch)" ]
+	then
+		touch="$(which touch)"
+	else
+		print_err "${BOLD}touch${NORMAL} is not installed. You need it to run this."
+	fi	
+	if [ -e "$(which echo)" ]
+	then
+		echo="$(which echo)"
+	else
+		print_err "${BOLD}echo${NORMAL} is not installed. You need it to run this."
+	fi	
 }
 
 print_err() {
 	if [ $# -eq 1 ]
 	then
-		echo -e "${ERR} $1"
+		"$echo" -e "${ERR} $1"
 	else
-		echo -e "${ERR} Unknown error occured!"
+		"$echo" -e "${ERR} Unknown error occured!"
 	fi
 }
 
 print_ok() {
 	if [ $# -eq 1 ]
 	then
-		echo -e "${OK} $1"
+		"$echo" -e "${OK} $1"
 	else
-		echo -e "${OK} Everything is fine!"
+		"$echo" -e "${OK} Everything is fine!"
 	fi
 }
 
 print_warning() {
 	if [ $# -eq 1 ]
 	then
-		echo -e "${WRN} $1"
+		"$echo" -e "${WRN} $1"
 	else
-		echo -e "${WRN} Something idk happenening!"
+		"$echo" -e "${WRN} Something idk happenening!"
 	fi
 }
 
@@ -86,7 +132,7 @@ arg_err() {
 	print_err "Missing or extra argument.\n"
 	if [ $# -eq 1 ]
 	then
-		help_command $1
+		help_command "$1"
 	else
 		help_command
 	fi
@@ -114,13 +160,13 @@ confirm() {
 }
 
 initialize() {
-	mkdir -p $YapmanPath $YapmanCachePath $YapmanLogsPath $YapmanPackagePath && print_ok "Necessary folders has been created."
-	touch $YapmanConfigPath && print_ok "Config file has been successfully created."
+	"$mkdir" -p "$YapmanCachePath" "$YapmanLogsPath" "$YapmanPackagePath" "$XDG_CONFIG_HOME" && print_ok "Necessary folders has been created."
+	"$touch" "$YapmanConfigPath" && print_ok "Config file has been successfully created."
 	print_ok "Initialized yapman. Enjoy now!.."
 }
 
 init() {
-	if [ -d $YapmanPath ]
+	if [ -d "$YapmanCachePath" ] | [ -e "$YapmanConfigPath" ] | [ -d "$YapmanLogsPath" ] | [ -d "$YapmanPackagePath" ]
 	then
 		print_warning "Already initialized."
 		if confirm "Re-initialize?"
@@ -136,23 +182,24 @@ init() {
 }
 
 version() {
-	echo -e $VERSION
+	"$echo" -e $VERSION
 }
 
 usage() {
-	echo -e "${BOLD}USAGE:${NORMAL}\nyapman.sh <operation> [...]"
-	echo -e "${BOLD}Operations:${NORMAL}"
-	echo -e "  {init}"
-	echo -e "  {-u update}"
-	echo -e "  {-i install} <package(s)>"
-	echo -e "  {-g get} <package(s)>"
-	echo -e "  {info} <package(s)>"
-	echo -e "  {-r remove} <package(s)>"
-	echo -e "  {-s search} <package(s)>"
-	echo -e "  {-c clear-cache}"
-	echo -e "  {-v version}"
-	echo -e "  {-h help}"
-	echo -e "\nUse 'yapman.sh {-h help}' with an operation for available options."
+	"$echo" -e "${BOLD}USAGE:${NORMAL}\nyapman.sh <operation> [...]
+${BOLD}Operations:${NORMAL}
+  {init}
+  {-u update}
+  {-i install} <package(s)>
+  {-g get} <package(s)>
+  {-a info} <package(s)>
+  {-r remove} <package(s)>
+  {-s search} <package(s)>
+  {-c clear-cache}
+  {-v version}
+  {-h help}
+
+Use 'yapman.sh {-h help}' with an operation for available options."
 }
 
 help_command() {
@@ -160,7 +207,7 @@ help_command() {
 	then
 		case $1 in
 			"search_package")
-				echo -e ""
+				"$echo" -e ""
 				;;
 			*)
 				usage
@@ -174,29 +221,29 @@ help_command() {
 clone_repo() {
 	if [ $# -eq 1 ]
 	then
-		git clone "${AUR_DOMAIN}/${1}.git" && return 0 || return 1
+		"$git" clone -q "${AUR_DOMAIN}/${1}.git" && return 0 || return 1
 	fi
 }
 
 install_package() {
 	if [ $# -ge 1 ]
 	then
-		for package in $@
+		for package in "$@"
 		do
 			print_ok "Getting ready to install ${BOLDB}${package}${NORMAL}"
 			local cache_path="${YapmanCachePath}/install_${package}.json"
-			curl -s -o $cache_path "${AUR_INFO_URL}${package}"
-			local status=$(jq -r '.type' $cache_path)
+			curl -s -o "$cache_path" "${AUR_INFO_URL}${package}"
+			local status=$("$jq" -r '.type' "$cache_path")
 			if [ "$status" = "error" ]
 			then
-				print_err "$(jq -r '.error' $cache_path)"
-			elif [ "$status" = "multiinfo" ]
+				print_err "$("$jq" -r '.error' "$cache_path")"
+			elif [ "$status" = "multiinfo" ] && [ -d "$YapmanPackagePath" ]
 			then
-				cd $YapmanPackagePath
-				local package_base="$(jq -r '.results[0].PackageBase' $cache_path)"
-				if clone_repo $package_base
+				cd "$YapmanPackagePath" || print_err "We couldn't find: ${YapmanPackagePath}" && exit 1
+				local package_base="$("$jq" -r '.results[0].PackageBase' "$cache_path")"
+				if clone_repo "$package_base"
 				then
-					cd $package_base
+					cd "$package_base" || exit 1
 					local clean
 					local remove
 					local syncdeps
@@ -218,7 +265,7 @@ install_package() {
 					else
 						syncdeps="s"
 					fi
-					makepkg -${syncdeps}i${remove}${clean}
+					"$makepkg" -${syncdeps}i${remove}${clean}
 				else
 					print_err "Something we did not calculated happened.  :/"
 					exit 1
@@ -234,21 +281,25 @@ install_package() {
 	fi
 }
 
-get_package_info() {
+get_package() {
 	if [ $# -ge 1 ]
 	then
-		for package in $@
+		for package in "$@"
 		do
-			print_ok "Getting info 4 ${BOLDB}${package}${NORMAL}"
-			local cache_path="${YapmanCachePath}/info_${package}.json"
-			curl -s -o $cache_path "${AUR_INFO_URL}${package}"
-			local status=$(jq -r '.type' $cache_path)
+			print_ok "Grabbing ${BOLDB}${package}${NORMAL}..."
+			local cache_path
+			cache_path="${YapmanCachePath}/get_${package}.json"
+			curl -s -o "$cache_path" "${AUR_INFO_URL}${package}"
+			local status
+			status="$("$jq" -r '.type' "$cache_path")"
 			if [ "$status" = "error" ]
 			then
-				print_err "$(jq -r '.error' $cache_path)"
+				print_err "$("$jq" -r '.error' "$cache_path")"
 			elif [ "$status" = "multiinfo" ]
 			then
-				echo -e "$(jq -r '.results[] | to_entries[] | "\u001b[1m\(.key):\u001b[0m \(.value)"' $cache_path)"
+				cd "$YapmanPackagePath" || print_err "We couldn't find: ${YapmanPackagePath}" && exit 1
+				local package_base="$("$jq" -r '.results[0].PackageBase' "$cache_path")"
+				clone_repo "$package_base" && print_ok "Done!.."
 			else
 				print_err "We've encountered some unexpected results."
 				exit 1
@@ -259,21 +310,87 @@ get_package_info() {
 	fi
 }
 
+get_package_info() {
+	if [ $# -ge 1 ]
+	then
+		for package in "$@"
+		do
+			print_ok "Getting info 4 ${BOLDB}${package}${NORMAL}"
+			local cache_path
+			cache_path="${YapmanCachePath}/info_${package}.json"
+			curl -s -o "$cache_path" "${AUR_INFO_URL}${package}"
+			local status
+			status="$("$jq" -r '.type' "$cache_path")"
+			if [ "$status" = "error" ]
+			then
+				print_err "$("$jq" -r '.error' "$cache_path")"
+			elif [ "$status" = "multiinfo" ]
+			then
+				"$echo" -e "$("$jq" -r '.results[] | to_entries[] | "\u001b[1m\(.key):\u001b[0m \(.value)"' "$cache_path")"
+			else
+				print_err "We've encountered some unexpected results."
+				exit 1
+			fi
+		done
+	else
+		arg_err
+	fi
+}
+
+remove_package() {
+	if [ $# -ge 1 ]
+	then
+		for package in "$@"
+		do
+			print_warning "Removing ${BOLDB}${package}${NORMAL}"
+			if confirm "Are you sure?"
+			then
+				local recursive
+				if confirm "Remove unnecessary dependencies, too?"
+				then
+					if confirm "Remove explicit dependencies, too?"
+					then
+						recursive="ss"
+					else
+						recursive="s"
+					fi
+				else
+					recursive=""
+				fi
+				local nofile
+				if confirm "Remove packages depends on $package?"
+				then
+					nofile="c"
+				else
+					nofile=""
+				fi
+				"$sudo" "$pacman" "-R${recursive}${nofile}"
+			else
+				print_err "Abort."
+			fi
+		done
+	else
+		arg_err
+	fi
+}
+
 search_package() {
 	if [ $# -ge 1 ]
 	then
-		for package in $@
+		for package in "$@"
 		do
 			print_ok "Searhing ${BOLDB}${package}${NORMAL}"
-			local cache_path="${YapmanCachePath}/search_results_${package}.json"
-			curl -s -o $cache_path "${AUR_SEARCH_URL}${package}"
-			local status=$(jq -r '.type' $cache_path)
+			local cache_path
+			cache_path="${YapmanCachePath}/search_results_${package}.json"
+			curl -s -o "$cache_path" "${AUR_SEARCH_URL}${package}"
+			local status
+			status=$("$jq" -r '.type' "$cache_path")
 			if [ "$status" = "error" ]
 			then
-				print_err "$(jq -r '.error' $cache_path)"
+				print_err "$("$jq" -r '.error' "$cache_path")"
 			elif [ "$status" = "search" ]
 			then
-				echo -e "${BOLD}$(jq -r '.results[] | .PackageBase' $cache_path)${NORMAL}"
+				"$echo" -e "${BOLD}$("$jq" -r '.results[] | .PackageBase' "$cache_path")${NORMAL}"
 			else
 				print_err "We've encountered some unexpected results."
 				exit 1
@@ -286,27 +403,23 @@ search_package() {
 
 clear_cache() {
 	print_warning "Cache files will be cleaned. Be careful!"
-	rm -rf $YapmanCachePath
-	if [ -e $YapmanCachePath ]
+	"$rm" -rf "$YapmanCachePath"
+	if [ -e "$YapmanCachePath" ]
 	then
 		print_err "Could not delete the cache files."
-		echo -e "You could try to remove them by yourself."
-		echo -e "Cache files is here, take a look: ${YapmanCachePath}"
+		"$echo" -e "You could try to remove them by yourself.
+Cache files is here, take a look: ${YapmanCachePath}"
 	else
 		print_ok "Cache files cleaned."
-		echo -e "What a clean space. Yum yum yum... :D"
-		mkdir $YapmanCachePath
+		"$echo" -e "What a clean space. Yum yum yum... :D"
+		"$mkdir" "$YapmanCachePath"
 	fi
 }
 
 load_config() {
-	if [ -e $YapmanConfigPath ]
+	if [ -e "$YapmanConfigPath" ]
 	then
-		source $YapmanConfigPath
-		if [ "$check_dependency_every_time" = "true" ]
-		then
-			can_run && echo
-		fi
+		source "$YapmanConfigPath"
 		if [ "$colorful_output" = "false" ]
 		then
 			BOLD="\033[1m"
@@ -338,41 +451,40 @@ load_config() {
 }
 
 main() {
+	can_run
 	load_config
-	if [ $# -ge 1 ]
+	if [ "$#" -ge 1 ]
 	then
 		if [ "$1" != "-v" ] && [ "$1" != "version" ]
 		then
 			intro
 		fi
-		case $1 in
-			"-v" | "version") version;;
-			"-h" | "help") 
-				help_command
-				;;
+		case "$1" in
+			"init") init;;
+			"-u" | "update") update;;
 			"-i" | "install")
-				install_package ${@:2}
+				install_package "${@:2}"
+				;;
+			"-g" | "get")
+				get_package "${@:2}"
+				;;
+			"-a" | "info")
+				get_package_info "${@:2}"
+				;;
+			"-r" | "remove")
+				remove_package ${@:2}
 				;;
 			"-s" | "search")
-				search_package ${@:2}
+				search_package "${@:2}"
 				;;
-			"info")
-				get_package_info ${@:2}
-				;;
-			"init")
-				init
-				;;
-  			"-c" | "clear-cache")
-			  	clear_cache
-				;;
-			*)
-				arg_err
-				;;
+  			"-c" | "clear-cache") clear_cache;;
+			"-v" | "version") version;;
+			"-h" | "help") help_command;;
+			*) arg_err;;
 		esac
 	else
-		intro
-		arg_err
+		intro && arg_err
 	fi
 }
 
-main $@
+main "$@"
